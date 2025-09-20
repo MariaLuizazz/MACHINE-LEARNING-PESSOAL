90931e83e6e57b229345b5a5c5de6311d3f33c4e
MariaLuizazz <souzaluizamaria57@gmail.com>
2025-09-20 19:06:38 -0300

    changes

--- docs/KNN/knn.py
+++ docs/KNN/knn.py
@@ -36,7 +36,7 @@ y = df['diagnosis']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
 #Treianamento do KNN
-knn = KNeighborsClassifier(n_neighbors=7)
+knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train, y_train)
 
 
